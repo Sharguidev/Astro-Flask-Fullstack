90064d3e2de8649ea9504d909f5b2827e69cf092
Sharguidev <guillermoj.obando@gmail.com>
2025-04-09 22:31:25 -0600

    Address field added to user table

--- src/backend/models.py
+++ src/backend/models.py
@@ -9,6 +9,7 @@ class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
+    adress: Mapped[str] = mapped_column(String(500), nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
 
